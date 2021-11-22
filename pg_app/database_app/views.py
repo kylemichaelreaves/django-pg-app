a2df2c4fd478e3518e4780f9b2b32ddf6c702e88
Kyle <kylereaves@gmail.com>
2021-11-21 21:41:28 -0500

    Removing obscene boilerplate that only a scoundrel would write.

--- pg_app/database_app/views.py
+++ pg_app/database_app/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('How dare you be a buttkiss, a heiny, sucking dick and cock!')
+    return HttpResponse('Hello, Index')
