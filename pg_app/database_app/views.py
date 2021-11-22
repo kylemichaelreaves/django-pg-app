7cb2b967774a59115e12034435e802761c7ac582
Kyle <kylereaves@gmail.com>
2021-11-21 21:46:17 -0500

    Getting the branch up to date

--- pg_app/database_app/views.py
+++ pg_app/database_app/views.py
@@ -3,8 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-<<<<<<< HEAD
     return HttpResponse('Hello, Index')
-=======
-    return HttpResponse('Django Landing Page')
->>>>>>> 7bb0f8976161c2c1e458e49b5e21b34c25ce2ee7
