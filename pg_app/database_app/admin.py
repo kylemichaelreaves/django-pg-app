c1a6596c1193b44a1612e7eeabf40bf504561337
Kyle <kylereaves@gmail.com>
2021-11-28 18:25:45 -0500

    Django config; tying up loose ends.

--- pg_app/database_app/admin.py
+++ pg_app/database_app/admin.py
@@ -1,8 +1,34 @@
 from django.contrib import admin
 from .models import Property
 
+"""
+    street_address
+    owner_name
+    owner_mailing_address
+    city_state_zip
+    owner_full_mailing_address
+    property_full_address
+    units_at_property
+    number_properties_owned
+    list_properties_owned
+    g_code
+    latitude
+    longitude
+"""
+
 # Register your models here.
 class PropertyAdmin(admin.ModelAdmin):
-    list_display = ('street_address', 'owner_name', 'number_associated_properties', 'list_associated_properties')
+    list_display = ('street_address',
+                    'owner_name',
+                    'owner_mailing_address',
+                    'city_state_zip',
+                    'owner_full_mailing_address',
+                    'property_full_address',
+                    'units_at_property',
+                    'number_associated_properties',
+                    'list_associated_properties',
+                    'g_code',
+                    'latitude',
+                    'longitude')
 
 admin.site.register(Property, PropertyAdmin)
--- pg_app/database_app/views.py
+++ pg_app/database_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import viewsets
+from .serializers import PropertySerializer
+from .models import Property
 
 # Create your views here.
-def index(request):
-    return HttpResponse('Hello, Index')
+class PropertyView(viewsets.ModelViewSet):
+    serializer_class = PropertySerializer
+    queryset = Property.objects.all()
