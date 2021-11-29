8a6c4dd275cf7873a4ea9281b680806e0f61bb3c
Kyle <kylereaves@gmail.com>
2021-11-28 20:19:14 -0500

    Conflict between duplicate models resolved.
    All duplicate models removed.

--- pg_app/database_app/urls.py
+++ pg_app/database_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from django.contrib import admin
 from rest_framework import routers
-from pg_app import views
+from database_app import views
 
 router = routers.DefaultRouter()
 router.register(r'pg_app',  views.PropertyView, 'property')
--- pg_app/pg_app/settings.py
+++ pg_app/pg_app/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'pg_app.database_app.apps.DatabaseAppConfig'
+    # 'pg_app.database_app.apps.DatabaseAppConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
