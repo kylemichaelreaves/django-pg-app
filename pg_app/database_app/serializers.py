90898956bf3fae12b2f345d4a98c3b9039510644
Kyle <kylereaves@gmail.com>
2021-12-04 10:58:13 -0500

    Still trying to load .csv into my Property model
    …Making progress. Property model updated with 'property_id' primary key.
    Initial migration remade to reflect those changes in the model.
    Serializer updated property_id.
    load_properties updated to capture property_id.
    Autogenerated index removed from 0001_initial.py.

--- pg_app/database_app/serializers.py
+++ pg_app/database_app/serializers.py
@@ -5,15 +5,18 @@ from .models import Property
 class PropertySerializer(serializers.ModelSerializer):
     class Meta:
         model = Property
-        fields = ('street_address',
-                  'owner_name',
-                  'owner_mailing_address',
-                  'city_state_zip',
-                  'owner_full_mailing_address',
-                  'property_full_address',
-                  'units_at_property',
-                  'g_code',
-                  'latitude',
-                  'longitude',
-                  'number_associated_properties',
-                  'list_associated_properties',)
+        fields = (
+            "property_id",
+            "street_address",
+            "owner_name",
+            "owner_mailing_address",
+            "city_state_zip",
+            "owner_full_mailing_address",
+            "property_full_address",
+            "units_at_property",
+            "g_code",
+            "latitude",
+            "longitude",
+            "number_associated_properties",
+            "list_associated_properties",
+        )
